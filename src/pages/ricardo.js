9ac8e78a52f2d6f6c372da65cf095b9b97e9ed1b
Kelebra <ricardokelebra@gmail.com>
2023-09-18 20:03:58 -0600

    feat: page contact

---
+++ src/pages/ricardo.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Contact } from "../components/Contact/Contact"
+
+export  const Eiichi = () => {
+
+    return (
+       <>
+        <Contact 
+            title="Ricardo Basurto"
+            phone="+52 5512431147"
+            email="ricardokelebra@gmail.com"
+            img="../../img/ricardo.jpeg"
+            alt="Ricardo Basurto"
+        />
+       </>
+    )
+}
+
+export default Eiichi
