c55df4e6d269876236b17e3a6a86bb93cf6949b5
Kelebra <ricardokelebra@gmail.com>
2023-09-18 20:03:58 -0600

    feat: page contact

---
+++ src/pages/contact-us-a.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Contact } from "../components/ContactA/ContactA"
+
+export  const Ariel = () => {
+
+    return (
+       <>
+        <Contact 
+            title="Ariel Garcia"
+            phone="+52 5544887906"
+            email="arielgarcia960715@gmail.com"
+            img="../../img/ricardo.jpeg"
+            alt="Ariel Garcia"
+        />
+       </>
+    )
+}
+
+export default Ariel
